b165ce0d60290c54e6f6da385f852560cbaffb3e
tong <tong@MacBookPro.mshome.net>
2026-01-11 16:28:14 +0800

    init: 项目初始化

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <div>
+    HelloWorld Component:
+  </div>
+</template>
+
+<style scoped>
+
+</style>
